e68b9977311104efdfacdbc12a80bd85a3af6ed4
sriley <sarahriley.dev@gmail.com>
2019-08-30 18:30:08 -0400

    refactor fetchPosts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,12 +1,10 @@
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  return async function(dispatch, getState) {
-    const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({
-      type: "FETCH_POSTS",
-      payload: response
-    });
-  };
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response
+  });
 };
