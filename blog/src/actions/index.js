fbbea03d2a1bfb25059ee2ad2f74b154638b7e17
sriley <sarahriley.dev@gmail.com>
2019-08-30 18:28:55 -0400

    add redux thunk middleware

---
+++ blog/src/actions/index.js
@@ -0,0 +1,12 @@
+import jsonPlaceholder from "../api/jsonPlaceholder";
+
+export const fetchPosts = () => {
+  return async function(dispatch, getState) {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({
+      type: "FETCH_POSTS",
+      payload: response
+    });
+  };
+};
