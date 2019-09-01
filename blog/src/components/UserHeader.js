28cc94a4208ff16070fc9901e1ccc1371dff8dae
sriley <sarahriley.dev@gmail.com>
2019-09-01 09:42:37 -0400

    refactor mapStateToProps for single user

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -8,11 +8,19 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    return <div>User Header</div>;
+    const { user } = this.props;
+    if (!user) {
+      return null;
+    }
+    return <div className="header">{user.name}</div>;
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchUser }
 )(UserHeader);
